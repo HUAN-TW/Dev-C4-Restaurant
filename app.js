df36d6fab3040af6fe9acace9678dcbddc1a1310
HUAN-TW <fb000326@gmail.com>
2023-12-21 18:19:23 +0800

    新增add頁面

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//Todo 新增一家餐廳 POST restaurants/new
+//Todo 新增一家餐廳 POST restaurants/add
 //Todo 餐廳的詳細資訊 GET restaurants/:id
 //Todo 瀏覽全部所有餐廳 GET restaurants
 //Todo 修改一家餐廳的資訊 PUT restaurants/:id/edit
@@ -28,6 +28,15 @@ app.get('/restaurant', (req, res) => {
     }
   )
 })
+
+app.get('/restaurants/add', (req, res) => {
+  return Restaurant.findAll({ attribute: ['category'], raw: true }).then(
+    (category) => {
+      res.render('add', { category })
+    }
+  )
+})
+
 //顯示restaurant detail
 app.get('/restaurants/:id', (req, res) => {
   const id = req.params.id
@@ -36,18 +45,6 @@ app.get('/restaurants/:id', (req, res) => {
       res.render('show', { Restaurant })
     }
   )
-})
-
-app.post('/restaurants/new', (req, res) => {
-  res.sned('add new restaurant')
-})
-
-app.put('/restaurants/:id/edit', (req, res) => {
-  res.send('modify restaurant')
-})
-
-app.delete('/restaurants/:id', (req, res) => {
-  res.send(`delete ${{ id }}`)
 })
 
 app.listen(port, () => {
